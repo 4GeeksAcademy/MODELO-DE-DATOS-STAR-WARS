ba624cffd5957341d3beef757019a7aae16a3037
danipadi13 <159074064+danipadi13@users.noreply.github.com>
2025-09-20 19:48:43 +0000

    odelo de datos star wars corregido

--- src/models.py
+++ src/models.py
@@ -1,19 +1,63 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker
+from eralchemy2 import render_er
 
-db = SQLAlchemy()
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+Base = declarative_base()
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), nullable=False)
+    name = Column(String(120), nullable=False)
+    lastname = Column(String(120), nullable=False)
+
+    favorites = relationship("Favorite", back_populates="user", cascade="all, delete-orphan")
+
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    birth_year = Column(String(20))
+    gender = Column(String(20))
+
+    favorites = relationship("Favorite", back_populates="character")
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    climate = Column(String(50))
+   
+    favorites = relationship("Favorite", back_populates="planet")
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    model = Column(String(120))
+    
+
+    favorites = relationship("Favorite", back_populates="vehicle")
+
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
+
+   
+    user = relationship("User", back_populates="favorites")
+    character = relationship("Character", back_populates="favorites")
+    planet = relationship("Planet", back_populates="favorites")
+    vehicle = relationship("Vehicle", back_populates="favorites")
+
+
+render_er(Base, 'diagram.png')
+
